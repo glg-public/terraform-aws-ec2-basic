b75ae310b15b6c61d5944f96fa28af767581b896
Satish Tripathi <stripathi@glgroup.com>
2023-08-03 14:29:58 +0530

    removed the null-label module

--- outputs.tf
+++ outputs.tf
@@ -25,7 +25,7 @@ output "id" {
 
 output "name" {
   description = "Instance name"
-  value       = module.label.id
+  value       = var.tags.name
 }
 
 output "ssh_key_pair" {
--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "default" {
   count       = local.security_group_count
-  name        = module.label.id
+  name        = var.tags.name
   vpc_id      = var.vpc_id
   description = "Instance default security group (only egress access is allowed)"
   tags        = var.tags
